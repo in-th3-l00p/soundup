fbcb9d1bf7283dc420daca28edbab5e4c4a03f45
intheloop <indianucode0303@gmail.com>
2023-02-02 14:27:26 +0200

    spring initializr

---
+++ src/main/java/com/troubleshooters/soundup/SoundupApplication.java
@@ -0,0 +1,13 @@
+package com.troubleshooters.soundup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/troubleshooters/soundup/SoundupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troubleshooters.soundup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
